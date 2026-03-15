d12ccc7947f47760e00e7492e8d3aaf3deee24bd
BuShiZm <danielganbarov6@gmail.com>
2026-03-15 06:25:10 +0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request, send_from_directory
-from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
-import os
-import json
-
-app = Flask(__name__, static_folder='public')
-
-SHEET_ID = '1AcrjqHJMi
