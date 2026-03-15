892dc263fa233b2e55d343c5d351babf5c4d73ab
BuShiZm <danielganbarov6@gmail.com>
2026-03-15 05:50:10 +0300

    Delete get-categories.js

--- get-categories.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const categories = ['Хоррор', 'Выживание', 'Инди', 'Мультиплеер', 'Сюжетная', 'Другое'];
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json(categories);
-}
