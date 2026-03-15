873b76c2ecc5e3f5923b5279c4cd763536ad64fa
BuShiZm <danielganbarov6@gmail.com>
2026-03-15 07:21:22 +0300

    Create vote-suggestion.js

---
+++ api/vote-suggestion.js
@@ -0,0 +1,16 @@
+import { google } from 'googleapis';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+
+    const sheets = google.sheets({ version: 'v4', auth });
