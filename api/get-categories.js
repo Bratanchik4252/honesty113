638a3eae5ce9a93499e37c3479ca3fd3509f6298
BuShiZm <danielganbarov6@gmail.com>
2026-03-15 07:43:39 +0300

    Update get-categories.js

--- api/get-categories.js
+++ api/get-categories.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const categories = ['Хоррор', 'Выживание', 'Инди', 'Мультиплеер', 'Сюжетная', 'Другое'];
+  const categories = ['Игра', 'Фильм', 'Сериал', 'Аниме', 'Другое'];
   res.status(200).json(categories);
 }
