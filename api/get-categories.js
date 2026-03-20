232736bee494b76ebe8aed715a5e3bdfa12fd36c
BuShiZm <danielganbarov6@gmail.com>
2026-03-20 22:15:53 +0300

    Delete api directory

--- api/get-categories.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  const categories = ['Игра', 'Фильм', 'Сериал', 'Аниме', 'Другое'];
-  res.status(200).json(categories);
-}
